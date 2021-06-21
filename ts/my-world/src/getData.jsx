7c411368b1e32e6df65b5a93bbedef669fd40c48
kaito101006 <85086944+kaito101006@users.noreply.github.com>
2021-06-22 03:11:57 +0900

    Add files via upload

---
+++ ts/my-world/src/getData.jsx
@@ -0,0 +1,18 @@
+export const gData =
+[
+    {
+        "ID":"1",
+        "Code":"0000",
+        "password":"password"
+    },
+    {
+        "ID":"2",
+        "Code":"0001",
+        "password":"abc123"
+    },
+    {
+        "ID":"999",
+        "Code":"err",
+        "password":"err"
+    }
+]
